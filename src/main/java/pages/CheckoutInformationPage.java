abf31324ddf4bd2c0db6587f069d030b3cbaad1a
test <test>
2026-01-19 12:47:59 +0100

    CheckoutInformationPage.java
    - added new elements and methods related to emptyRequiredFieldsTest

--- src/main/java/pages/CheckoutInformationPage.java
+++ src/main/java/pages/CheckoutInformationPage.java
@@ -3,13 +3,18 @@ package pages;
 import data.CheckoutData;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import lombok.Getter;
 import org.openqa.selenium.WebElement;
 
+@Getter
 public class CheckoutInformationPage extends BasePage {
 
     public CheckoutInformationPage(AppiumDriver driver) {
         super(driver);
     }
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"CHECKOUT: INFORMATION\")")
+    private WebElement checkoutInformationHeader;
 
     @AndroidFindBy(accessibility = "test-First Name")
     private WebElement firstNameInput;
@@ -20,12 +25,33 @@ public class CheckoutInformationPage extends BasePage {
     @AndroidFindBy(accessibility = "test-Zip/Postal Code")
     private WebElement zipCodeInput;
 
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"First Name is required\")")
+    private WebElement firstNameErrorText;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Last Name is required\")")
+    private WebElement lastNameErrorText;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Postal Code is required\")")
+    private WebElement zipCodeErrorText;
+
     @AndroidFindBy(accessibility = "test-CONTINUE")
     private WebElement continueButton;
 
     public void fillCheckoutInformation(CheckoutData checkoutData) {
+        fillFirstName(checkoutData);
+        fillLastName(checkoutData);
+        fillZipCode(checkoutData);
+    }
+
+    public void fillFirstName(CheckoutData checkoutData) {
         sendKeys(firstNameInput, checkoutData.getFirstName());
+    }
+
+    public void fillLastName(CheckoutData checkoutData) {
         sendKeys(lastNameInput, checkoutData.getLastName());
+    }
+
+    public void fillZipCode(CheckoutData checkoutData) {
         sendKeys(zipCodeInput, checkoutData.getZipCode());
     }
 
